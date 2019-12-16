e5222d749ee347473e9957acfbb4b34deb976452
blutooth <maximilian@live.co.uk>
2019-12-16 17:07:42 +0000

    changes

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,6 +6,8 @@
 
 
 
+
+
 (function($) {
 
 	var	$window = $(window),
@@ -37,6 +39,15 @@
 			}, 100);
 		});
 
+    function hideLoader() {
+        $('#loading').hide();
+    }
+    
+    $(window).ready(hideLoader);
+
+    // Strongly recommended: Hide loader after 20 seconds, even if the page hasn't finished loading
+    setTimeout(hideLoader, 20 * 1000);
+    
 	// Dropdowns.
 		$('#nav > ul').dropotron({
 			mode: 'fade',
